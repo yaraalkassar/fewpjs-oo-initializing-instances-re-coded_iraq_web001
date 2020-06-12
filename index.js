4fa2b6425a48785ff4c42589f9ff1d01eb129871
Yara Alkassar <yara.sarmad@yahoo.com>
2020-06-12 17:10:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,3 +5,18 @@ class Breakfast{
     this.drink = drink;
   }
 }
+
+class Lunch{
+  constructor(salad, soup, drink){
+    this.salad = salad;
+    this.soup = soup;
+    this.drink = drink;
+  }
+}
+
+class Dinner{
+  constructor(food, drink){
+    this.food = food;
+    this.drink = drink;
+  }
+}
